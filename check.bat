6c7bac249fc3079bbe41999b45951a5da1c7b4f5
Sebastian Kraft <admin@helfull.de>
2020-06-01 19:09:13 +0200

    add better information text if something odd is detected

--- check.bat
+++ check.bat
@@ -9,5 +9,10 @@ goto exit
 
 :error
 echo failed check
+echo It is most likly that you are infected by the anarchy grabber
+echo Please uninstall discord and reinstall it
+echo for more information about it check
+echo https://www.bleepingcomputer.com/news/security/discord-client-turned-into-a-password-stealer-by-updated-malware/?fbclid=IwAR0coRGdRgB-iGs0iVRpcuUYVEXSGCaj0ZPYj586Kn0G52P2oW5ALsb5k-k
+pause
 
 :exit
